44e2bc62356d578e024bb486a7080a09853599a2
Zach Schwartz <zischwartz@gmail.com>
2016-03-02 09:54:23 -0500

    that seems to work

--- src/ios/SwiftVideoPlugin.swift
+++ src/ios/SwiftVideoPlugin.swift
@@ -2,21 +2,33 @@ import Foundation
 
 @objc(SwiftVideoPlugin) class SwiftVideoPlugin : CDVPlugin {
   func sayHello(command: CDVInvokedUrlCommand) {
-    let message = "Hello !";
+  	commandDelegate?.runInBackground({() -> Void in
+	    let message = "Hello !";
+	    // print(CDVInvokedUrlCommand)
+	    let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: message);
+	    pluginResult.setKeepCallbackAsBool(true)
+	    self.commandDelegate?.sendPluginResult(pluginResult, callbackId:command.callbackId);
 
-    let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: message);
-    pluginResult.setKeepCallbackAsBool(true)
-    commandDelegate?.sendPluginResult(pluginResult, callbackId:command.callbackId);
-
-    let pluginResultTwo = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "World!");
-    // pluginResultTwo.setKeepCallbackAsBool(true) // this will cause it not to work,
-    // if you set this true (again)
-    // you need to then send another result after the one below
-    commandDelegate?.sendPluginResult(pluginResultTwo, callbackId:command.callbackId);
-
+	    let pluginResultTwo = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "World!");
+	    // pluginResultTwo.setKeepCallbackAsBool(true) // this will cause it not to work,
+	    // if you set this true (again)
+	    // you need to then send another result after the one below
+	    self.commandDelegate?.sendPluginResult(pluginResultTwo, callbackId:command.callbackId);
+	 })
 
   }
 }
 
 
 // http://stackoverflow.com/questions/25448976/how-to-write-cordova-plugin-in-swift
+
+// func myPluginMethod(command: CDVInvokedUrlCommand) {
+//     // Check command.arguments here.
+//     self.commandDelegate.runInBackground({() -> Void in
+//         var payload: String? = nil
+//             // Some blocking logic...
+//         var pluginResult: CDVPluginResult = CDVPluginResult.resultWithStatus(CDVCommandStatus_OK, messageAsString: payload!)
+//         // The sendPluginResult method is thread-safe.
+//         self.commandDelegate.sendPluginResult(pluginResult, callbackId: command.callbackId)
+//     })
+// }
