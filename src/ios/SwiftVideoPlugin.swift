e8cad30f730006b96ea0746efc70fef20ba747bd
Zach Schwartz <zischwartz@gmail.com>
2016-03-04 10:45:40 -0500

    threaded

--- src/ios/SwiftVideoPlugin.swift
+++ src/ios/SwiftVideoPlugin.swift
@@ -20,89 +20,68 @@ import GPUImage
   var lanczosResamplingFilter: GPUImageLanczosResamplingFilter = GPUImageLanczosResamplingFilter()
 
   func sayHello(command: CDVInvokedUrlCommand) {
-        self.videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPreset352x288, cameraPosition: .Back)
-        self.videoCamera!.outputImageOrientation = .Portrait;
+  		self.commandDelegate?.runInBackground({() -> Void in
 
-        self.lanczosResamplingFilter.forceProcessingAtSize(CGSizeMake(35, 28))
+	        self.videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPreset352x288, cameraPosition: .Back)
+	        self.videoCamera!.outputImageOrientation = .Portrait;
 
-        // stillImageSource.addTarget(lanczosResamplingFilter)
-        // lanczosResamplingFilter.useNextFrameForImageCapture()
-        // stillImageSource.processImage()
-        // var lanczosImage: UIImage = lanczosResamplingFilter.imageFromCurrentFramebuffer()
+	        self.lanczosResamplingFilter.forceProcessingAtSize(CGSizeMake(35, 28))
+
+	        // stillImageSource.addTarget(lanczosResamplingFilter)
+	        // lanczosResamplingFilter.useNextFrameForImageCapture()
+	        // stillImageSource.processImage()
+	        // var lanczosImage: UIImage = lanczosResamplingFilter.imageFromCurrentFramebuffer()
 
 
-        // filter = GPUImagePixellateFilter()
-        // videoCamera?.addTarget(filter)
-        // videoCamera?.addTarget(filter)
-        // filter?.addTarget(self.view as! GPUImageView)
+	        // filter = GPUImagePixellateFilter()
+	        // videoCamera?.addTarget(filter)
+	        // videoCamera?.addTarget(filter)
+	        // filter?.addTarget(self.view as! GPUImageView)
 
-//        Works
-        // self.output = GPUImageRawDataOutput(imageSize: CGSizeMake(35, 28), resultsInBGRAFormat: true)
-        self.output = GPUImageRawDataOutput(imageSize: CGSizeMake(352, 288), resultsInBGRAFormat: true)
-//        this works too, slightly slower it seems? unlike guy says
-//        output = MyRawDataOutput(imageSize: CGSizeMake(352, 288), resultsInBGRAFormat: true)
+	//        Works
+	        // self.output = GPUImageRawDataOutput(imageSize: CGSizeMake(35, 28), resultsInBGRAFormat: true)
+	        self.output = GPUImageRawDataOutput(imageSize: CGSizeMake(352, 288), resultsInBGRAFormat: true)
+	//        this works too, slightly slower it seems? unlike guy says
+	//        output = MyRawDataOutput(imageSize: CGSizeMake(352, 288), resultsInBGRAFormat: true)
 
-        self.output!.newFrameAvailableBlock = { () in
-            let since_last = ((NSDate().timeIntervalSinceReferenceDate-self.last_time.timeIntervalSinceReferenceDate)*1000)
-            if since_last > 500 {
-	            // print(since_last)
-	            self.rawBytesForImage = self.output!.rawBytesForImage
-	            // print("raw raw mem:")
-	            // print(self.rawBytesForImage!.memory)
-	            self.last_time = NSDate()
-	           	// print(self.rawBytesForImage?.memory)
-	            //http://stackoverflow.com/a/31109955/83859
-	            // let buffer = UnsafeMutableBufferPointer(start: self.rawBytesForImage!, count: Int(352*288*4)) // also WORKS
-	            // let array = [GLubyte](buffer)
-	            // print(array[0], array[1])
+	        self.output!.newFrameAvailableBlock = { () in
+	            let since_last = ((NSDate().timeIntervalSinceReferenceDate-self.last_time.timeIntervalSinceReferenceDate)*1000)
+	            if since_last > 500 {
+		            // print(since_last)
+		            self.rawBytesForImage = self.output!.rawBytesForImage
+		            // print("raw raw mem:") // print(self.rawBytesForImage!.memory)
+		            self.last_time = NSDate()
+					var data = NSData(bytes: self.rawBytesForImage!, length: Int(352*288*4)) // also WORKS
+					// http://stackoverflow.com/a/24516400/83859
+					// let count = data.length / sizeof(UInt8) // all should be uint8, but uint seems to work with anyobject? 
+					// create array of appropriate length:
+					// var array = [UInt8](count: count, repeatedValue: 0) // but cordova wants AnyObject gah
+					// copy bytes into array
+					// data.getBytes(&array, length:count * sizeof(UInt8))
+					// print(array)
+					// Works!
+					// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: array.map({"\($0)"}).joinWithSeparator(",")) // buffer
+					// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hey hey h!!!!!ey!") // buffer
+					// This seems like it would work, but for `Cannot convert value of type '[UInt8]' to expected argument type '[AnyObject]!'`
+					// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsArray: array) // or buffer or data
 
-	            // http://stackoverflow.com/questions/25997376/type-string-anyobject-does-not-conform-to-protocol-anyobject-why
-	            // print(buffer[0], buffer[1], buffer[2])
-	            // https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/TypeCasting.html
+					
+					// Works, need to use a DataView in JS! Hooray/duh.
+					let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsArrayBuffer: data) // or buffer or data
 
-				var data = NSData(bytes: self.rawBytesForImage!, length: Int(352*288*4)) // also WORKS
-				// var data_string = NSString(data: data, encoding: NSUTF8StringEncoding) as? String
+					pluginResult.setKeepCallbackAsBool(true)
 
-				
-				// http://stackoverflow.com/a/24516400/83859
-				let count = data.length / sizeof(UInt8) // all should be uint8, but uint seems to work with anyobject? 
-				// // // create array of appropriate length:
-				var array = [UInt8](count: count, repeatedValue: 0) // but cordova wants AnyObject gah
-				// // // copy bytes into array
-				data.getBytes(&array, length:count * sizeof(UInt8))
-				// print(array.map({"\($0)"}).joinWithSeparator("-"))
-				// print(str)
-				// print(array.joinWithSeparator("-"))
-				// array.joinWithSeparator("-")
-				// print(array)
+					self.commandDelegate?.sendPluginResult(pluginResult, callbackId:command.callbackId)
+	            }
+	        }
+	//        https://github.com/BradLarson/GPUImage/blob/master/framework/Source/GPUImageLookupFilter.h
+	//        answer: http://stackoverflow.com/questions/33731637/how-to-create-gpuimage-lookup-png-resource
+	        // self.videoCamera?.addTarget(self.lanczosResamplingFilter)
+	        self.videoCamera?.addTarget(self.output)
+	        self.videoCamera?.startCameraCapture()
 
-				// print(data.bytes.memory)
-				// let ptr = UnsafePointer<UInt8>(data.bytes)
-
-				// let data = NSData(bytes: self.rawBytesForImage!, length: Int(35*28*4)) // also WORKS
-				print("frame")
-
-				// Works!
-				// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: array.map({"\($0)"}).joinWithSeparator(",")) // buffer
-
-				// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hey hey h!!!!!ey!") // buffer
-				// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsArrayBuffer: &data) // or buffer or data
-				
-				// Sends an empty array buffer? Gahh
-				let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsArrayBuffer: data) // or buffer or data
-
-				// This seems like it would work, but for `Cannot convert value of type '[UInt8]' to expected argument type '[AnyObject]!'`
-				// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsArray: array) // or buffer or data
-				pluginResult.setKeepCallbackAsBool(true)
-
-				self.commandDelegate?.sendPluginResult(pluginResult, callbackId:command.callbackId)
-            }
-        }
-//        https://github.com/BradLarson/GPUImage/blob/master/framework/Source/GPUImageLookupFilter.h
-//        answer: http://stackoverflow.com/questions/33731637/how-to-create-gpuimage-lookup-png-resource
-        // self.videoCamera?.addTarget(self.lanczosResamplingFilter)
-        self.videoCamera?.addTarget(self.output)
-        self.videoCamera?.startCameraCapture()
+	     })// end runsinbackground
+        
         //            buffer.baseAddress.destroy(buffer.count)
         // 
 
@@ -122,3 +101,5 @@ import GPUImage
 
 
 // http://stackoverflow.com/questions/25448976/how-to-write-cordova-plugin-in-swift
+
+
