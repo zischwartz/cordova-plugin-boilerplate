39b0a6352cfe38d57f9bea39f61cbdc4662ceca6
Zach Schwartz <zischwartz@gmail.com>
2016-03-08 12:25:33 -0500

    cleaning up

--- notes.md
+++ notes.md
@@ -1,4 +1,4 @@
-Notes:
+## Notes
 
 http://stackoverflow.com/questions/27496055/getting-file-not-found-in-bridging-header-when-importing-objective-c-framework
 
@@ -10,3 +10,46 @@ http://stackoverflow.com/questions/25538579/use-cocoapods-with-an-app-extension
 
 http://stackoverflow.com/questions/25414404/how-to-bundle-cocoapod-dependencies-with-cordova-plugin
 https://github.com/xdissent/cocoapods-cordova
+
+Size/Resolution 
+https://en.wikipedia.org/wiki/Common_Intermediate_Format
+
+// AVCaptureVideoOrientationLandscapeRight
+// .LandscapeRight
+// self.videoCamera.setInputRotation(.GPUImageRotateRight, atIndex:0)
+
+// WORKS
+// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: array.map({"\($0)"}).joinWithSeparator(","))
+
+// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hey hey h!!!!!ey!") // buffer
+// This seems like it would work, but for `Cannot convert value of type '[UInt8]' to expected argument type '[AnyObject]!'`
+// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsArray: array) // or buffer or data
+
+
+
+// http://stackoverflow.com/a/24516400/83859
+var count = data.length / sizeof(UInt8) // all should be uint8, but uint seems to work with anyobject? 
+// print(self.destSize)
+// print(count)
+//405 504 with no filter
+// 3920 with 35*28
+// create array of appropriate length:
+// var array = [UInt8](count: count, repeatedValue: 0) // but cordova wants AnyObject gah
+// copy bytes into array
+// data.getBytes(&array, length:count * sizeof(UInt8))
+// print(array)
+// Works!
+
+
+//        https://github.com/BradLarson/GPUImage/blob/master/framework/Source/GPUImageLookupFilter.h
+//        answer: http://stackoverflow.com/questions/33731637/how-to-create-gpuimage-lookup-png-resource
+// But fuck that
+
+
+This works, but I don't think it's neccesary. 
+
+// self.lanczosResamplingFilter = GPUImageLanczosResamplingFilter()
+// self.lanczosResamplingFilter?.forceProcessingAtSize(self.destSize!)
+// self.videoCamera?.addTarget(self.lanczosResamplingFilter)
+// self.lanczosResamplingFilter?.addTarget(self.output)
+// self.videoCamera?.startCameraCapture()
--- src/ios/SwiftVideoPlugin.swift
+++ src/ios/SwiftVideoPlugin.swift
@@ -31,91 +31,47 @@ struct Options {
   }
 
   func setupCamera(command: CDVInvokedUrlCommand) {
-  	// print(command.arguments)
   	let js_opt = command.arguments[0]
-  	// print(js_opt)
-  	// print(js_opt["width"])
- //  	let width: Int = js_opt["width"]
- //  	let height: Int = js_opt["height"]
   	self.options.size = CGSize(width: js_opt["width"] as! Int, height: js_opt["height"] as! Int)
-  	self.options.minMsPerFrame = js_opt["ms"] as! Int
- //  	// print(self.options.size)
- //  	// print(js_opt)
- //  	// print(js_opt.ms)
-
-
-  	print(self.options)
+  	self.options.minMsPerFrame = js_opt["ms"] as? Int
+  	// print(self.options)
   	let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
   	self.commandDelegate?.sendPluginResult(pluginResult, callbackId:command.callbackId)
   }
 
   func begin(command: CDVInvokedUrlCommand) {
   		self.commandDelegate?.runInBackground({() -> Void in
-
+            // TODO allow for for other presets/resolutons
 	        self.videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPreset352x288, cameraPosition: .Back)
 	        self.videoCamera!.outputImageOrientation = .Portrait;
-	        self.lanczosResamplingFilter = GPUImageLanczosResamplingFilter()
-	        // self.destSize = CGSize(width: 352, height: 288)
-	        self.destSize = CGSize(width: 352/2, height: 288/2)
-	        // self.destSize = CGSize(width: 128, height: 96)
-	        // 128 × 96 ? https://en.wikipedia.org/wiki/Common_Intermediate_Format
-	        //  ????????????
-	        // AVCaptureVideoOrientationLandscapeRight
-	        // .LandscapeRight
-	        // self.videoCamera.setInputRotation(.GPUImageRotateRight, atIndex:0)
-
-	//        Works
-	        // self.output = GPUImageRawDataOutput(imageSize: CGSizeMake(35, 28), resultsInBGRAFormat: true)
-	        self.output = GPUImageRawDataOutput(imageSize: self.destSize!, resultsInBGRAFormat: true)
-	        // self.output = GPUImageRawDataOutput(imageSize: self.size, resultsInBGRAFormat: true)
+            // Sizes evidently need to be from 
+            // https://en.wikipedia.org/wiki/Common_Intermediate_Format
+	        self.output = GPUImageRawDataOutput(imageSize: self.options.size!, resultsInBGRAFormat: true)
 
 	        self.output!.newFrameAvailableBlock = { () in
 	            let since_last = ((NSDate().timeIntervalSinceReferenceDate-self.last_time.timeIntervalSinceReferenceDate)*1000)
-	            // 300 was good at 352, 2888
-	            // 50 for "/2 "/2
-	            // 0 at 128, 96
+                // XXX if this is too low, javascript cb won't ever happen
+	            // 300 was good at 352, 288
+	            // 50 for 176, 144
+	            // 0 worked at 128, 96
 	            if since_last > 50 { 
-		            // print(since_last)
+                    self.last_time = NSDate()
 		            self.rawBytesForImage = self.output!.rawBytesForImage
-		            // print("raw raw mem:") // print(self.rawBytesForImage!.memory)
-		            self.last_time = NSDate()
-					// var data = NSData(bytes: self.rawBytesForImage!, length: Int(self.destSize.width*self.destSize.height*4)) 
-					var data = NSData(bytes: self.rawBytesForImage!, length: Int(self.destSize!.width*self.destSize!.height*4)) 
-					// var data = NSData(bytes: self.rawBytesForImage!, length: Int(35*28*4)) 
-					// http://stackoverflow.com/a/24516400/83859
+					var data = NSData(bytes: self.rawBytesForImage!, length: Int(self.options.size!.width*self.options.size!.height*4)) 
 					var count = data.length / sizeof(UInt8) // all should be uint8, but uint seems to work with anyobject? 
-					// print(self.destSize)
-					// print(count)
-					//405 504 with no filter
-					// 3920 with 35*28
-					// create array of appropriate length:
-					// var array = [UInt8](count: count, repeatedValue: 0) // but cordova wants AnyObject gah
-					// copy bytes into array
-					// data.getBytes(&array, length:count * sizeof(UInt8))
-					// print(array)
-					// Works!
-					// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: array.map({"\($0)"}).joinWithSeparator(",")) // buffer
-					// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Hey hey h!!!!!ey!") // buffer
-					// This seems like it would work, but for `Cannot convert value of type '[UInt8]' to expected argument type '[AnyObject]!'`
-					// let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsArray: array) // or buffer or data
+                    // print(count)
 
-					
-					// Works, need to use a DataView in JS! Hooray/duh.
+					// This creates `data` an ArrayBuffer on the js side. 
+                    // To use it, you'll probably need a DataView 
 					let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsArrayBuffer: data) // or buffer or data
-
 					pluginResult.setKeepCallbackAsBool(true)
-
 					self.commandDelegate?.sendPluginResult(pluginResult, callbackId:command.callbackId)
 	            }
 	        }
-	//        https://github.com/BradLarson/GPUImage/blob/master/framework/Source/GPUImageLookupFilter.h
-	//        answer: http://stackoverflow.com/questions/33731637/how-to-create-gpuimage-lookup-png-resource
 
-	        
-	        // self.lanczosResamplingFilter?.forceProcessingAtSize(self.destSize!)
-	        // self.videoCamera?.addTarget(self.lanczosResamplingFilter)
-	        // self.lanczosResamplingFilter?.addTarget(self.output)
-	        // self.videoCamera?.startCameraCapture()
+
+
+
 
 	        self.videoCamera?.addTarget(self.output)
 	        self.videoCamera?.startCameraCapture()
